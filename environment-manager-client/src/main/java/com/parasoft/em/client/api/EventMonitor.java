09fb866563739835c53bfd011d734d697a003eab
Daniel Dominguez <daniel@parasoft.com>
2013-09-25 14:12:05 -0700

    Added event monitoring ability in the client

---
+++ environment-manager-client/src/main/java/com/parasoft/em/client/api/EventMonitor.java
@@ -0,0 +1,20 @@
+/*
+ * $RCSfile$
+ * $Revision$
+ *
+ * Comments:
+ *
+ * (C) Copyright ParaSoft Corporation 2013.  All rights reserved.
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF ParaSoft
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * $Author$          $Locker$
+ * $Date$
+ * $Log$
+ */
+package com.parasoft.em.client.api;
+
+public interface EventMonitor {
+    void logMessage(String message);
+}
