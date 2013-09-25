2f4b54bd7bd812085e7db7d99e2c11c19d7d00f0
Daniel Dominguez <daniel@parasoft.com>
2013-09-25 14:57:11 -0700

    Test url and add default context as a backup url

--- environment-manager-ant-plugin/src/main/java/com/parasoft/environmentmanager/ant/ProvisionTask.java
+++ environment-manager-ant-plugin/src/main/java/com/parasoft/environmentmanager/ant/ProvisionTask.java
@@ -22,8 +22,10 @@ import net.sf.json.JSONObject;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
+import com.parasoft.em.client.api.Environments;
 import com.parasoft.em.client.api.EventMonitor;
 import com.parasoft.em.client.api.Provisions;
+import com.parasoft.em.client.impl.EnvironmentsImpl;
 import com.parasoft.em.client.impl.ProvisionsImpl;
 
 public class ProvisionTask extends Task {
@@ -78,6 +80,27 @@ public class ProvisionTask extends Task {
 
     @Override
     public void execute() throws BuildException {
+        // First test the url
+        try {
+            Environments envs = new EnvironmentsImpl(url, username, password);
+            envs.getEnvironments();
+        } catch (IOException e) {
+            // Try to append the default context path '/em'
+            String testUrl = url;
+            try {
+                if (testUrl.endsWith("/")) {
+                    testUrl += "em";
+                } else {
+                    testUrl += "/em";
+                }
+                Environments envs = new EnvironmentsImpl(testUrl, username, password);
+                envs.getEnvironments();
+                url = testUrl;
+            } catch (IOException e1) {
+                throw new BuildException("Could not connect to Environment Manager at " + url);
+            }
+        }
+        
         try {
             Provisions provisions = new ProvisionsImpl(url, username, password);
             JSONObject event = provisions.createProvisionEvent(environmentId, instanceId, abortOnFailure);
