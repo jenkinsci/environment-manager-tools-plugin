aa6ed1b7cb1a36d3a12ca2b1585cd0d6aed42670
Daniel Dominguez <daniel@parasoft.com>
2013-09-26 06:32:22 -0700

    Initial commit

---
+++ environment-manager-maven-plugin/src/main/java/com/parasoft/environmentmanager/maven/EnvironmentManagerMojo.java
@@ -0,0 +1,106 @@
+/*
+ * $RCSfile$
+ * $Revision$
+ *
+ * Comments:
+ *
+ * (C) Copyright ParaSoft Corporation 2013.  All rights reserved.
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF ParaSoft
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * $Author$          $Locker$
+ * $Date$
+ * $Log$
+ */
+package com.parasoft.environmentmanager.maven;
+
+import java.io.IOException;
+
+import net.sf.json.JSONObject;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import com.parasoft.em.client.api.Environments;
+import com.parasoft.em.client.api.EventMonitor;
+import com.parasoft.em.client.api.Provisions;
+import com.parasoft.em.client.impl.EnvironmentsImpl;
+import com.parasoft.em.client.impl.ProvisionsImpl;
+
+public class EnvironmentManagerMojo extends AbstractMojo {
+
+    /**
+     * @parameter expression="${em.url}"
+     * @required
+     */
+    private String url;
+    
+    /**
+     * @parameter expression="${em.username}"
+     */
+    private String username;
+    
+    /**
+     * @parameter expression="${em.password}"
+     */
+    private String password;
+    
+    /**
+     * @parameter expression="${em.environmentId}"
+     * @required
+     */
+    private int environmentId;
+    
+    /**
+     * @parameter expression="${em.instanceId}"
+     * @required
+     */
+    private int instanceId;
+    
+    /**
+     * @parameter expression="${em.abortOnFailure}"
+     */
+    private boolean abortOnFailure;
+    
+    
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        // First test the url
+        try {
+            Environments envs = new EnvironmentsImpl(url, username, password);
+            envs.getEnvironments();
+        } catch (IOException e) {
+            // Try to append the default context path '/em'
+            String testUrl = url;
+            try {
+                if (testUrl.endsWith("/")) {
+                    testUrl += "em";
+                } else {
+                    testUrl += "/em";
+                }
+                Environments envs = new EnvironmentsImpl(testUrl, username, password);
+                envs.getEnvironments();
+                url = testUrl;
+            } catch (IOException e1) {
+                throw new MojoExecutionException("Could not connect to Environment Manager at " + url, e);
+            }
+        }
+        
+        try {
+            Provisions provisions = new ProvisionsImpl(url, username, password);
+            JSONObject event = provisions.createProvisionEvent(environmentId, instanceId, abortOnFailure);
+            boolean success = provisions.monitorEvent(event, new EventMonitor() {
+                public void logMessage(String message) {
+                    getLog().info(message);
+                }
+            });
+            if (!success) {
+                throw new MojoFailureException("Provisioning event failed");
+            }
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not connect to Environment Manager at " + url, e);
+        }
+    }
+
+}
